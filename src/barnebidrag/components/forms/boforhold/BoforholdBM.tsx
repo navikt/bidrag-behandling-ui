cfd96dbc1c1001d4e847486d7ea87b075aa80f51
markodraksic <draksic.m@gmail.com>
2025-03-17 09:20:06 +0100

    Add table for new BMs boforhold opplysninger and activation button

--- src/barnebidrag/components/forms/boforhold/BoforholdBM.tsx
+++ src/barnebidrag/components/forms/boforhold/BoforholdBM.tsx
@@ -1,24 +1,38 @@
-import { BostatusperiodeGrunnlagDto } from "@api/BidragBehandlingApiV1";
+import { BostatusperiodeGrunnlagDto, OpplysningerType } from "@api/BidragBehandlingApiV1";
 import { Rolletype } from "@api/BidragDokumentProduksjonApi";
 import PersonNavnIdent from "@common/components/PersonNavnIdent";
 import { RolleTag } from "@common/components/RolleTag";
 import elementIds from "@common/constants/elementIds";
 import text from "@common/constants/texts";
+import { useBehandlingProvider } from "@common/context/BehandlingContext";
 import { useGetBehandlingV2 } from "@common/hooks/useApiData";
+import { useOnActivateGrunnlag } from "@common/hooks/useOnActivateGrunnlag";
+import { useVirkningsdato } from "@common/hooks/useVirkningsdato";
 import { hentVisningsnavn } from "@common/hooks/useVisningsnavn";
-import { BodyShort, Box, ExpansionCard, Table } from "@navikt/ds-react";
-import { dateOrNull, DateToDDMMYYYYString } from "@utils/date-utils";
+import { BodyShort, Box, Button, ExpansionCard, Heading, HStack, Table, Tag } from "@navikt/ds-react";
+import { dateOrNull, DateToDDMMYYYYString, isBeforeDate } from "@utils/date-utils";
 import React, { Fragment } from "react";
 
 export const BoforholdBM = () => {
     const {
         aktiveGrunnlagsdata: { husstandsmedlemBM },
+        ikkeAktiverteEndringerIGrunnlagsdata: { husstandsmedlemBM: ikkeAktiverteEndringerHusstandsmedlemBM },
     } = useGetBehandlingV2();
+    const harNyeOpplysninger = ikkeAktiverteEndringerHusstandsmedlemBM.some(
+        (husstandsmedlem) => !!husstandsmedlem.perioder.length
+    );
 
     return (
         <ExpansionCard size="small" aria-label="Small-variant">
             <ExpansionCard.Header>
-                <ExpansionCard.Title size="small">{text.title.opplysningerFraFolkeregistret}</ExpansionCard.Title>
+                <ExpansionCard.Title size="small">
+                    {text.title.opplysningerFraFolkeregistret}{" "}
+                    {harNyeOpplysninger && (
+                        <Tag size="xsmall" variant="success" className="ml-2">
+                            {text.label.nytt}
+                        </Tag>
+                    )}
+                </ExpansionCard.Title>
             </ExpansionCard.Header>
             <ExpansionCard.Content>
                 {husstandsmedlemBM.map((husstandsmedlem) => (
@@ -37,6 +51,7 @@ export const BoforholdBM = () => {
                                 </div>
                             </div>
                             <Perioder perioder={husstandsmedlem.perioder} />
+                            <NyOpplysningerFraFolkeregistreTabell ident={husstandsmedlem.ident} />
                         </Box>
                     </Fragment>
                 ))}
@@ -79,3 +94,87 @@ const Perioder = ({ perioder }: { perioder: BostatusperiodeGrunnlagDto[] }) => {
         </Table>
     );
 };
+
+function NyOpplysningerFraFolkeregistreTabell({ ident }: { ident: string }) {
+    const {
+        ikkeAktiverteEndringerIGrunnlagsdata: { husstandsmedlemBM },
+    } = useGetBehandlingV2();
+    const ikkeAktivertePerioder = husstandsmedlemBM.find(
+        (husstandsmedlem) => husstandsmedlem.ident === ident
+    )?.perioder;
+    const virkningsOrSoktFraDato = useVirkningsdato();
+    const { setSaveErrorState } = useBehandlingProvider();
+    const activateGrunnlag = useOnActivateGrunnlag();
+
+    const onActivate = () => {
+        activateGrunnlag.mutation.mutate(
+            {
+                overskriveManuelleOpplysninger: false,
+                personident: ident,
+                grunnlagstype: OpplysningerType.BOFORHOLDBMSOKNADSBARN,
+            },
+            {
+                onSuccess: (response) => {
+                    activateGrunnlag.queryClientUpdater((currentData) => {
+                        return {
+                            ...currentData,
+                            aktiveGrunnlagsdata: response.aktiveGrunnlagsdata,
+                            ikkeAktiverteEndringerIGrunnlagsdata: response.ikkeAktiverteEndringerIGrunnlagsdata,
+                        };
+                    });
+                },
+                onError: () => {
+                    setSaveErrorState({
+                        error: true,
+                        retryFn: () => onActivate(),
+                    });
+                },
+            }
+        );
+    };
+
+    if (!ikkeAktivertePerioder || ikkeAktivertePerioder.length === 0) return null;
+
+    return (
+        <Box
+            padding="4"
+            background="surface-default"
+            borderWidth="1"
+            borderRadius="medium"
+            borderColor="border-default"
+            className="w-[708px]"
+        >
+            <Heading size="xsmall">{text.alert.nyOpplysningerBoforhold}</Heading>
+            <table className="mt-2">
+                <thead>
+                    <tr>
+                        <th align="left">{text.label.fraOgMed}</th>
+                        <th align="left">{text.label.tilOgMed}</th>
+                        <th align="left">{text.label.status}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {ikkeAktivertePerioder?.map((periode, index) => (
+                        <tr key={index + periode.datoFom}>
+                            <td width="100px" scope="row">
+                                {virkningsOrSoktFraDato && isBeforeDate(periode.datoFom, virkningsOrSoktFraDato)
+                                    ? DateToDDMMYYYYString(virkningsOrSoktFraDato)
+                                    : DateToDDMMYYYYString(new Date(periode.datoFom))}
+                            </td>
+                            <td width="100px">
+                                {" "}
+                                {periode.datoTom ? DateToDDMMYYYYString(new Date(periode.datoTom)) : ""}
+                            </td>
+                            <td width="250px">{hentVisningsnavn(periode.bostatus)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <HStack gap="6" className="mt-4">
+                <Button type="button" variant="secondary" size="xsmall" onClick={() => onActivate()}>
+                    Ja
+                </Button>
+            </HStack>
+        </Box>
+    );
+}
